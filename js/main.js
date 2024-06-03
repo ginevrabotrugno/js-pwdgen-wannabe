8c43fabd38473be08c1028ef4fd8fda050a41eff
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-03 16:22:37 +0200

    add password generator

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// PASSWORD GENERATOR
+
+
+// Chiedi all’utente il suo nome
+let nomeUtente = prompt ("Inserisci qui il tuo nome");
+
+
+// chiedi all'utente il suo cognome
+let cognomeUtente = prompt ("Inserisci qui il tuo cognome");
+
+
+// chiedi all'utente il suo colore preferito
+
+let colorePreferito = prompt ("Inserisci qui il tuo colore preferito");
+
+// scrivi sulla pagina nomecognomecolorepreferito23
+
+console.log( "La nuova password è: " + nomeUtente + cognomeUtente + colorePreferito + "23");
